a4f2d1dddb7859e9f852465163a424cef481674a
Jose R. Gonzalez <komish@flutes.dev>
2025-05-30 13:22:55 -0500

    Add testing -  internal/ansible
    Signed-off-by: Jose R. Gonzalez <komish@flutes.dev>

---
+++ internal/ansible/ansible_suite_test.go
@@ -0,0 +1,13 @@
+package ansible_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+func TestAnsible(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Ansible Suite")
+}
