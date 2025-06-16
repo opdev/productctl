d9e7dc6995ef5d591df59852453ae0c284f6f0c6
Jose R. Gonzalez <komish@flutes.dev>
2025-06-16 16:06:55 -0500

    Add utilities to cleanup product listings or components in one-off scenarios
    Signed-off-by: Jose R. Gonzalez <komish@flutes.dev>

--- internal/cmd/productctl/cmd/cmd.go
+++ internal/cmd/productctl/cmd/cmd.go
@@ -9,9 +9,11 @@ import (
 
 	"github.com/opdev/productctl/internal/cli"
 	"github.com/opdev/productctl/internal/cmd/productctl/cmd/apply"
+	"github.com/opdev/productctl/internal/cmd/productctl/cmd/archivecomponent"
 	"github.com/opdev/productctl/internal/cmd/productctl/cmd/bridge"
 	"github.com/opdev/productctl/internal/cmd/productctl/cmd/cleanup"
 	"github.com/opdev/productctl/internal/cmd/productctl/cmd/create"
+	"github.com/opdev/productctl/internal/cmd/productctl/cmd/deleteproductlisting"
 	"github.com/opdev/productctl/internal/cmd/productctl/cmd/fetch"
 	"github.com/opdev/productctl/internal/cmd/productctl/cmd/jsonschema"
 	"github.com/opdev/productctl/internal/cmd/productctl/cmd/sanitize"
@@ -35,6 +37,13 @@ func RootCmd() *cobra.Command {
 
 	cmd.AddCommand(version.Command())
 	cmd.PersistentFlags().String(cli.FlagIDLogLevel, "info", "The verbosity of the tool itself. Ex. error, warn, info, debug")
+
+	util := bridge.Command("util", "Utilities for the management of your Partner Connect account")
+	util.PersistentFlags().String(cli.FlagIDEndpoint, "prod", "The catalog API environment to use. Choose from stage, prod")
+	util.PersistentFlags().String(cli.FlagIDCustomEndpoint, "", "Define a custom API endpoint. Supersedes predefined environment values like \"prod\" if set")
+	util.AddCommand(archivecomponent.Command())
+	util.AddCommand(deleteproductlisting.Command())
+	cmd.AddCommand(util)
 
 	// Build the product management command tree.
 	product := bridge.Command("product", "Manage your Product Listing")
---
+++ internal/cmd/productctl/cmd/deleteproductlisting/deleteproductlisting.go
@@ -0,0 +1,72 @@
+package deleteproductlisting
+
+import (
+	"context"
+
+	"github.com/Khan/genqlient/graphql"
+	"github.com/spf13/cobra"
+
+	"github.com/opdev/productctl/internal/catalogapi"
+	"github.com/opdev/productctl/internal/cli"
+	"github.com/opdev/productctl/internal/genpyxis"
+	"github.com/opdev/productctl/internal/logger"
+)
+
+func Command() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "delete-productlisting <productID>",
+		Short: "Deletes the product listingwith the specified product ID.",
+		Long: `Deletes the product listing with the specified product ID
+
+This should be considered a destructive operation. Note that there are various reasons why the API may reject this operation. Those reasons may need to be handled directly via the Partner Connect UI.
+`,
+		Args: cobra.MinimumNArgs(1), // The product ID
+		RunE: runE,
+	}
+
+	return cmd
+}
+
+func runE(cmd *cobra.Command, args []string) error {
+	L := logger.FromContextOrDiscard(cmd.Context())
+	_, token, err := cli.EnsureEnv()
+	if err != nil {
+		return err
+	}
+
+	var endpoint string
+	if cmd.Flags().Changed(cli.FlagIDCustomEndpoint) {
+		endpoint, _ = cmd.Flags().GetString(cli.FlagIDCustomEndpoint)
+		L.Debug("custom endpoint set, using it over env value", "endpoint", endpoint)
+	} else {
+		env, _ := cmd.Flags().GetString(cli.FlagIDEndpoint)
+		endpoint, err = cli.ResolveAPIEndpoint(env)
+		if err != nil {
+			return err
+		}
+		L.Debug("endpoint resolved", "endpoint", endpoint)
+	}
+
+	return run(cmd.Context(), args[0], token, endpoint)
+}
+
+func run(ctx context.Context, listingID string, token string, endpoint catalogapi.APIEndpoint) error {
+	L := logger.FromContextOrDiscard(ctx)
+	L.Info("deleting product listing", "_id", listingID)
+
+	L.Debug("building graphql client")
+	httpClient := catalogapi.TokenAuthenticatedHTTPClient(token, L.With("name", "httpclient"))
+	client := graphql.NewClient(endpoint, httpClient)
+
+	resp, err := genpyxis.DeleteProduct(ctx, client, listingID)
+	if err != nil {
+		return err
+	}
+
+	if gqlErr := resp.Update_product_listing.GetError(); gqlErr != nil {
+		return catalogapi.ParseGraphQLResponseError(gqlErr)
+	}
+
+	L.Info("done")
+	return nil
+}
